5021ff38ace6f48c3a11d2a0b0639086b6558969
Dion Garman <diongarman@gmail.com>
2017-04-27 16:00:51 +0100

    sorted file paths out, added type2 charts and cleaned up redundant front end code

--- Django/Django-Chart.js-9deb1d489250aca706b9939ae0bad331d0709982/src/charts/views.py
+++ Django/Django-Chart.js-9deb1d489250aca706b9939ae0bad331d0709982/src/charts/views.py
@@ -1,41 +1,42 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.views.generic import View
-
 from django.http import JsonResponse, HttpResponse
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import numpy as np
 import pandas as pd
 
 #json formatting
 import json
 
-User = get_user_model()
+#File Paths
+from settings import BASE_DIR
 
+#GLOBALS
+pokemon_csv = BASE_DIR + '/pokemon-sun-and-moon-gen-7-stats/pokemon.csv'
 
-
+#HomeView contains ajax api calls to TableData and PokemonChartData
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'charts.html')
 
-
-df = pd.read_csv('/Users/diongarman/PycharmProjects/Django-Chart.js-9deb1d489250aca706b9939ae0bad331d0709982/src/pokemon-sun-and-moon-gen-7-stats/pokemon.csv', header=None)
-dataset = df.fillna('N/A').values.tolist() #fillna() function avoids invalid data being passed by view
 
 class TableData(APIView):
 
     authentication_classes = []
     permission_classes = []
 
+
+
     def get(self, request, format=None):
-        columns = json.dumps(dataset[0])
-        pokemon = json.dumps(dataset[1:])
+        df = pd.read_csv(pokemon_csv, header=None).fillna('N/A').values.tolist()
+
+        header = list(map(lambda col: {'title': col}, df[0]))
+        pokemon = json.dumps(df[1:])
 
         data = {
             "data": pokemon,
-            "columns": columns
+            "columns": header
         }
 
 
@@ -45,16 +46,21 @@ class PokemonChartData(APIView):
     #structure and format data for charts.js to display, aggregate, group etc.
 
     def get(self, request, format=None):
-        df2 = pd.read_csv(
-            '/Users/diongarman/PycharmProjects/Django-Chart.js-9deb1d489250aca706b9939ae0bad331d0709982/src/pokemon-sun-and-moon-gen-7-stats/pokemon.csv')
+        df2 = pd.read_csv(pokemon_csv)
 
         type_counts = df2['type1'].value_counts().tolist()
         types = df2['type1'].value_counts().index.tolist()
 
+        type2_counts = df2['type2'].value_counts().tolist()
+        types2 = df2['type2'].value_counts().index.tolist()
+
         data = {
             "types": types,
-            "type_counts": type_counts
+            "type_counts": type_counts,
+            "types2": types2,
+            "type2_counts": type2_counts
         }
+
 
         return Response(data)
 
